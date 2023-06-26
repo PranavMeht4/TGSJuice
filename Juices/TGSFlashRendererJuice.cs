8219c2c60100a8a2e6c35445f6b73da75b360f51
Pranav Mehta <pranav0123.pm@gmail.com>
2023-06-26 17:09:01 +0530

    added TGSPlayParticleJuice.cs and improved TGSFlashRendererJuice

--- Juices/TGSFlashRendererJuice.cs
+++ Juices/TGSFlashRendererJuice.cs
@@ -9,7 +9,6 @@ namespace TGSJuice
     public class TGSFlashRendererJuice : TGSJuiceBase
     {
         public Renderer TargetRenderer;
-        public string ColorPropertyName = "_Color";
         public Color FlashColor = Color.white;
         public float FlashDuration = 0.5f;
         public int RepeatCount = 0;
@@ -21,14 +20,7 @@ namespace TGSJuice
         private void Awake()
         {
             _mat = TargetRenderer.material;
-            if (_mat.HasProperty(ColorPropertyName))
-            {
-                _originalColor = _mat.GetColor(ColorPropertyName);
-            }
-            else
-            {
-                Debug.LogError("The material doesn't have a color property with the name: " + ColorPropertyName);
-            }
+            _originalColor = _mat.color;
         }
 
         public override void Play()
@@ -66,25 +58,21 @@ namespace TGSJuice
                     break;
                 }
             }
-
-            // Reset color to original after all flashing done
-            _mat.SetColor(ColorPropertyName, _originalColor);
         }
 
         private IEnumerator ChangeMaterialColor(Material material, Color targetColor, float duration)
         {
-            Color initialColor = material.GetColor(ColorPropertyName);
             float elapsedTime = 0f;
 
             while (elapsedTime < duration)
             {
                 elapsedTime += Time.deltaTime;
                 float t = Mathf.Clamp01(elapsedTime / duration);
-                material.SetColor(ColorPropertyName, Color.Lerp(initialColor, targetColor, t));
+                material.color = Color.Lerp(_originalColor, targetColor, t);
                 yield return null;
             }
 
-            material.SetColor(ColorPropertyName, targetColor);
+            _mat.color = _originalColor;
         }
     }
 }
---
+++ Juices/TGSPlayParticleJuice.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace TGSJuice
+{
+    [AddComponentMenu("")]
+    [JuiceLabel("Play Particle")]
+    [JuiceDescription("Play particle effect")]
+    public class TGSPlayParticleJuice : TGSJuiceBase
+    {
+        public ParticleSystem ParticleEffect;
+        public bool Loop = false;
+
+        private float _effectTimer;
+
+        public override void Play()
+        {
+            if (ParticleEffect == null) return;
+            ParticleSystem.MainModule mainModule = ParticleEffect.main;
+            mainModule.loop = Loop;
+
+            ParticleEffect.Play();
+        }
+    }
+}
